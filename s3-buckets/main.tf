f67aaf5140436136f783e9e893301e45585ce45b
Charles Dunda <cdunda@2u.com>
2023-10-03 16:25:15 -0400

    Add basic bucket terraform code

---
+++ s3-buckets/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_version = ">= 1.5.7"
+  backend "s3" {
+    bucket = "2u-terraform"
+    key    = "cdunda/port-demo/s3-buckets/terraform.tfstate"
+    region = "us-west-2"
+  }
+}
+
+provider "aws" {
+  region = var.region
+
+  assume_role {
+    role_arn = "arn:aws:iam::516590517042:role/OrganizationAccountAccessRole"
+  }
+
+  # Make it faster by skipping something
+  skip_metadata_api_check     = true
+  skip_region_validation      = true
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+}
+
+module "simple_bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "3.15.1"
+
+  bucket        = var.bucket_name
+  force_destroy = true
+}
