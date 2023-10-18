a35901bdfa7099f9f00255abb48c57712c2740b1
Charles Dunda <cdunda@2u.com>
2023-10-18 17:02:07 -0400

    make parse json script recursive

--- .github/scripts/parse_json.py
+++ .github/scripts/parse_json.py
@@ -2,17 +2,24 @@ import json
 import sys
 
 
-def try_parse_json(s):
-    try:
-        return json.loads(s)
-    except json.JSONDecodeError:
-        return s
+def try_parse_json_recursive(obj):
+    if isinstance(obj, str):
+        try:
+            obj = json.loads(obj)
+        except json.JSONDecodeError:
+            return obj
+
+    if isinstance(obj, dict):
+        for key, value in obj.items():
+            obj[key] = try_parse_json_recursive(value)
+    elif isinstance(obj, list):
+        for i, value in enumerate(obj):
+            obj[i] = try_parse_json_recursive(value)
+
+    return obj
 
 
 if __name__ == "__main__":
     data = json.load(sys.stdin)
-    for key, value in data.items():
-        if isinstance(value, str):
-            data[key] = try_parse_json(value)
-
+    data = try_parse_json_recursive(data)
     print(json.dumps(data, indent=4))
